e0ba3e87262dd9eae89e396985ab6410986475e4
unknown <carljacobdiazcruz@gmail.com>
2022-11-19 11:07:56 +0800

    Update: frontend package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+// pages
+import Products from "./pages/products";
+import ProductDetails from "./pages/product-details";
+import Cart from "./pages/cart";
+import Checkout from "./pages/checkout";
+import Orders from "./pages/orders";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Products />} />
+      <Route path="/product-details" element={<ProductDetails />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/orders" element={<Orders />} />
+    </Routes>
   );
 }
 
